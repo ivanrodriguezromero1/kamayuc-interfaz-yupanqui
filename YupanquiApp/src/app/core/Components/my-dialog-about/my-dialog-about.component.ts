5b318ba645b6f600ffca204e82d83eb76ed34439
Jackie <jackeline.ibanez.q@uni.pe>
2022-04-17 20:36:17 -0500

    toolbar

---
+++ YupanquiApp/src/app/core/Components/my-dialog-about/my-dialog-about.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-dialog-about',
+  templateUrl: './my-dialog-about.component.html',
+  styleUrls: ['./my-dialog-about.component.css']
+})
+export class MyDialogAboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
