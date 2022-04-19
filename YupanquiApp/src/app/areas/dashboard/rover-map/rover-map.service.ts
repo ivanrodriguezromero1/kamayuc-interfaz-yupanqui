b3228bc4d78294f909647d2feb8367b427052767
Italo <italo.rivadeneyra.r@uni.pe>
2022-04-18 21:09:53 -0500

    cambios de aspecto  y themming angular material

--- YupanquiApp/src/app/areas/dashboard/rover-map/rover-map.service.ts
+++ YupanquiApp/src/app/areas/dashboard/rover-map/rover-map.service.ts
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { ElementRef, Injectable, NgZone, OnDestroy } from '@angular/core';
+import { ElementRef, HostListener, Injectable, NgZone, OnDestroy } from '@angular/core';
 import { VRMLLoader } from 'node_modules/three/examples/jsm/loaders/VRMLLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 // import  Stats  from 'node_modules/three/examples/jsm/libs/stats.module.js';
@@ -15,14 +15,32 @@ export class EngineService implements OnDestroy {
   cube: THREE.Mesh;
   rover: any;
   objeto: any;
-  controls:any;
+  controls: any;
+  ejeX = new THREE.Vector3(1, 0, 0);
+  ejeY = new THREE.Vector3(0, 1, 0);
+  ejeZ = new THREE.Vector3(0, 0, 1);
+  girX = 0;
+  girY = 0;
+  girZ = 0;
   // stats = new Stats();
   private frameId: number = null;
 
-  constructor(private ngZone: NgZone) {
-    // this.stats = new Stats();
+  constructor(
+    private ngZone: NgZone
+  ) {
+    window.addEventListener("keydown", function (event) {
+      console.log(event.key);
 
+
+
+
+    }, true);
   }
+  //teclado down
+  // @HostListener('window:keydown', [])
+  // onWindowKeyDown() {
+  //   console.log('keydown')
+  // }
 
   ngOnDestroy(): void {
     if (this.frameId != null) {
@@ -35,6 +53,7 @@ export class EngineService implements OnDestroy {
     }
   }
 
+
   createScene(canvas: ElementRef<HTMLCanvasElement>): void {
     // The first step is to get the reference of the canvas element from our HTML document
     this.canvas = canvas.nativeElement;
@@ -42,23 +61,23 @@ export class EngineService implements OnDestroy {
     // canvas.nativeElement.appendChild( this.stats.dom );
     this.renderer = new THREE.WebGLRenderer({
       canvas: this.canvas,
-      alpha: false,    // transparent background
+      alpha: true,    // transparent background
       antialias: true // smooth edges
     });
-    this.renderer.setSize(800, 500);
+    this.renderer.setSize(window.innerWidth, 600);
 
     // create the scene
     this.scene = new THREE.Scene();
 
-    this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 20000);
+    this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / 600, 1, 20000);
 
-    this.camera.position.set( - 10, 5, 10 );
+    this.camera.position.set(-10, 7, -2);
 
 
     // light
 
     var dirLight = new THREE.DirectionalLight(0xeeeeee);
-    dirLight.position.set( 200, 200, 200 );
+    dirLight.position.set(200, 200, 200);
 
     this.camera.add(dirLight);
     this.camera.add(dirLight.target);
@@ -107,24 +126,35 @@ export class EngineService implements OnDestroy {
         // called when the resource is loaded
         console.log("Rover Cargado")
         console.log(object);
-
-        // object.rotation.x = 0;
-        // object.rotation.y = 0;
-        // object.rotation.z = 0;
-        // var ejeX = new THREE.Vector3(1, 0, 0);
-        // var ejeY = new THREE.Vector3(0, 1, 0);
-        // var ejeZ = new THREE.Vector3(0, 0, 1);
-        // var girX = 0;
-        // var girY = 0;
-        // var girZ = 0;
-        // object.rotateOnAxis(ejeX, girX);
-        // object.rotateOnAxis(ejeY, girY);
-        // object.rotateOnAxis(ejeZ, girZ);
         this.rover = object;
-        this.rover.scale.set(4, 4, 4);
-        this.rover.position.x = -4;//rotation x :-Math.PI/2
-        this.rover.position.y = 4;//0.76
+
+        this.rover.position.x = 0;//rotation x :-Math.PI/2
+        this.rover.position.y = 0.76;//0.76
         this.rover.position.z = 0;//0.02
+
+
+        this.rover.rotation.x = 0;
+        this.rover.rotation.y = 0;
+        this.rover.rotation.z = 0;
+
+        this.rover.rotateOnAxis(this.ejeX, this.girX);
+        this.rover.rotateOnAxis(this.ejeY, this.girY);
+        this.rover.rotateOnAxis(this.ejeZ, this.girZ);
+
+        this.rover.scale.set(4, 4, 4);
+
+        // for(let i=0;i<=120;i++){
+        // 	huellaGeo = new THREE.BoxGeometry( 1, 0.2, 1 );
+        // 	huellaMat = new THREE.MeshBasicMaterial( {color: 0x0CB6B8} );
+        // 	huellaMesh = new THREE.Mesh( huellaGeo, huellaMat );
+
+        // 	huellaMesh.position.x=0;
+        // 	huellaMesh.position.y=0;
+        // 	huellaMesh.position.z=0;
+        // 	arrayHuella[i]=huellaMesh;
+        // 	scene.add(huellaMesh);
+        // }
+
         this.scene.add(this.rover);
         this.scene.add(object)
       },
@@ -137,11 +167,11 @@ export class EngineService implements OnDestroy {
         console.error('An error happened', error);
       },
     );
-  //   loader.load('../../../../assets/vrm/rover.wrl', function(object){
-  //     alert(object);
-  //     this.scene.add(object);
-  //     console
-  // });
+    //   loader.load('../../../../assets/vrm/rover.wrl', function(object){
+    //     alert(object);
+    //     this.scene.add(object);
+    //     console
+    // });
   }
 
   animate(): void {
@@ -184,6 +214,6 @@ export class EngineService implements OnDestroy {
     this.camera.aspect = width / height;
     this.camera.updateProjectionMatrix();
 
-    this.renderer.setSize(width, height);
+    this.renderer.setSize(window.innerWidth, 600);
   }
 }
